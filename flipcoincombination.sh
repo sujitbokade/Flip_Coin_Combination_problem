b403cda4ff6ada08cc72afc6b93d35e70fc9f37d
sujit <sujitbokade2014@gmail.com>
2022-06-23 07:00:05 +0530

    [sujit]Singlet Combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,8 +1,25 @@
-#!/bin/bash
-toss=$(( RANDOM % 2 ))
-if(( toss == 0 ))
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi
+!/bin/bash
+read -p "Enter number of times to flip a combination:" no_of_flips
+
+echo 
+echo "SINGLET-COMBINATION"
+declare -A singlet_frequency=( 
+
+                                [H]=0
+                                [T]=0
+
+                              )
+for(( flip = 1; flip <= no_of_flips; flip++ ))
+do
+    echo -n "Flip-$flip is "
+    (( coin = RANDOM % 2 ))
+    case $coin in 
+        0)
+            echo "Heads"
+            (( singlet_frequency[H]++ ))
+            ;;
+        1)
+            echo "Tails"
+            (( singlet_frequency[T]++ ))
+    esac
+done
